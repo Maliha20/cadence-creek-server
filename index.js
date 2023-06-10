709d24730bc4b29ba0288a7146f3927fa0affd5e
maliha20 <gulista.marium20@gmail.com>
2023-06-10 20:46:16 +0600

    admin route made

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.use(cors())
 app.use(express.json())
 
 
+const verifyJwt =(req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true, message: 'Unauthorized Access'})
+  }
+const token = authorization.split(' ')[1];
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+  if(err){
+    return res.status(401).send({error:true, message: 'Unauthorized Access'})
+  }
+  req.decoded = decoded;
+  next()
+})
+}
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rtt8qfl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,13 +44,43 @@ async function run() {
     const instructorsCollection = client.db('musicDB').collection("instructors")
     
 
-    app.post('/jwt', (req,res)=>{
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, 
-        { expiresIn: '1h' })
-      res.send=({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+
+     //use verify jawt before verify admin
+
+     const verifyAdmin = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      if(user?.role !== 'admin'){
+        return res.status(403).send({error:true, message: 'forbidden access'})
+      }
+      next()
+     }
+
+     //FOR INSTRUCTORS
+     const verifyInstructor = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      if(user?.role !== 'instructor'){
+        return res.status(403).send({error:true, message: 'forbidden access'})
+      }
+      next()
+     }
+//verifyinstructor
+
+    app.get('/users', verifyJwt, verifyInstructor, async(req,res)=>{
+        const result = await usersCollection.find().toArray()
+        res.send(result)
     })
-    app.get('/users', async(req,res)=>{
+
+    //verifyadmin
+    app.get('/users', verifyJwt, verifyAdmin, async(req,res)=>{
         const result = await usersCollection.find().toArray()
         res.send(result)
     })
@@ -52,6 +96,35 @@ async function run() {
         res.send(result)
     })
 
+   
+    // const decodedEmail = req.decoded.email;
+    // if(email !==decodedEmail){
+    // return res.status(403).send({error:true, message: 'Access prohibited'})
+    // }
+
+    app.get("/users/admin/:email", verifyJwt,async(req, res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({admin:false})
+      }
+      const query = {email: email};
+     const user = await usersCollection.findOne(query)
+     const result = {admin : user?.role === "admin"}
+     res.send(result)
+    })
+
+    //instructor
+    app.get("/users/instructor/:email", verifyJwt,async(req, res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({instructor:false})
+      }
+      const query = {email: email};
+     const user = await usersCollection.findOne(query)
+     const result = {instructor : user?.role === "instructor"}
+     res.send(result)
+    })
+
     app.patch("/users/admin/:id",async(req, res)=>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
@@ -63,6 +136,7 @@ async function run() {
      const result = await usersCollection.updateOne(query , updatedDoc);
      res.send(result)
     })
+    //instructor
     app.patch("/users/instructor/:id",async(req, res)=>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
@@ -74,7 +148,8 @@ async function run() {
      const result = await usersCollection.updateOne(query , updatedDoc);
      res.send(result)
     })
-
+ 
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
